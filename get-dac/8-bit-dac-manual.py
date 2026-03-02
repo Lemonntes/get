334e701a37a1feab4f41143e84d7976ea8f5078a
Lemonntes <silanteva.ea@phystech.edu>
2026-03-02 19:15:10 +0300

    Update 8-bit-dac-manual.py

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,33 +1,30 @@
 import RPi.GPIO as GPIO
-
+pins = [16,20,21,25,26,17,27,22]
 GPIO.setmode(GPIO.BCM)
-q = [16,20,21,25,26,17,27,22]
-dynrange = 3.3
-GPIO.setup(q, GPIO.OUT)
-
-def vtn(v):  
-    if not (0.0 <= v <=dynrange):
-        print(f"напряжение выходит за динамический диапазон цап(0.00-{dynrange:.2f}B)")
-        print("Устанавливаем 0.0В")
+GPIO.setup(pins, GPIO.OUT)
+dynamic_range = 3.183
+def voltage_to_number(voltage):
+    if not (0<=voltage<=dynamic_range):
+        print(f'Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)')
+        print('Устанавливаем 0.0 В')
         return 0
-
-    return int(v/dynrange*255)
-
-def n2d(n):  # Изменено: параметр переименован с v на n для соответствия
+    return int(voltage/dynamic_range*255)
+def decimal2binary(n):
+    return [int(a) for a in bin(n)[2:].zfill(8)]
+def number_to_dac(number):
+    num = decimal2binary(number)
     for i in range(8):
-        GPIO.output(q[i], (n>>i)&1)  # Изменено: v заменено на n
+        GPIO.output(pins[i], num[i])
 
 try:
     while True:
         try:
-
-            v = float(input("Веедите напряжени в вольтах:"))
-            n = vtn(v)
-            n2d(n)
-
+            voltage = float(input('Введите напряжение в вольтах: '))
+            number = voltage_to_number(voltage)
+            number_to_dac(number)
         except ValueError:
-            print("Вы ввели не число, попробуйте еще раз\n")
+            print('Вы ввели не число. Попробуйте еще раз\n')
 
 finally:
-    GPIO.output(q,0)
+    GPIO.output(pins, 0)
     GPIO.cleanup()
