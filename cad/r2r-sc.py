ba6bf6a07adf3b0cfaa78029ba24ed8ae89299a6
Lemonntes <silanteva.ea@phystech.edu>
2026-03-02 12:12:10 +0300

    Update r2r-sc.py

--- cad/r2r-sc.py
+++ cad/r2r-sc.py
@@ -1,7 +1,21 @@
-import RPi GPIO as GPIO
 import time
+import adc_plot
+from r2r_adc import R2R_ADC
 
+try:
+    adc = R2R_ADC(3.290, compare_time=0.0001, verbose=False)
 
-voltage_values = []
-time_values = []
-duration = 3.0
+    voltage_values = []
+    time_values = []
+    duration = 3.0
+
+    start_time = time.time()
+
+    while time.time() - start_time < duration:
+        voltage_values.append(adc.get_sc_voltage())
+        time_values.append(time.time() - start_time)
+
+    adc_plot.plot_voltage_vs_time(time_values, voltage_values, 3.3)
+
+finally:
+    adc.deinit()
